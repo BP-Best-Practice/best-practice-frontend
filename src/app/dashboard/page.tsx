896431ac8ee0032150bcf5a0707c39b4c35bc526
Shion <seoy1108@gmail.com>
2025-08-13 21:45:48 +0900

    chore: 이미지 최적화(remotePatterns) 설정 추가 및 태그 변경

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import Link from 'next/link';
+import Image from 'next/image';
 import { useSession } from 'next-auth/react';
 import AuthButton from '@/components/AuthButton';
 
@@ -11,7 +12,13 @@ export default function Dashboard() {
     <div className="flex">
       <div className="flex flex-col gap-4 border border-black h-screen rounded-md p-4">
         <div className="flex items-center gap-2 mb-4">
-          <img className="rounded-full w-[2.8rem]" src={session?.user.image} />
+          <Image
+            className="rounded-full"
+            width={30}
+            height={30}
+            src={session?.user.image || '/profile.png'}
+            alt="Image of the user"
+          />
           <p>{session?.user.name}</p>
           <AuthButton />
         </div>
